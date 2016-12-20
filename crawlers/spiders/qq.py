856d1969fd39996665161c55e4b57109f66fcf2c
zephyr <qiulimao@getqiu.com>
2016-12-20 14:41:28 +0800

    fixed improper on ipad portrait display on search page;add serveral starts_url for qq news

--- crawlers/spiders/qq.py
+++ crawlers/spiders/qq.py
@@ -31,7 +31,9 @@ class QqSpider(Spider):
     start_urls=["http://news.qq.com","http://mil.qq.com/mil_index.htm","http://sports.qq.com/",
                 "http://ent.qq.com/","http://finance.qq.com/","http://stock.qq.com/",
                 "http://auto.qq.com/","http://tech.qq.com/","http://digi.tech.qq.com/",
-                "http://cd.house.qq.com/","http://bj.house.qq.com/","http://edu.qq.com/"]
+                "http://cd.house.qq.com/","http://bj.house.qq.com/","http://edu.qq.com/",
+                "http://news.qq.com/world_index.shtml","http://news.qq.com/society_index.shtml",
+                ]
     #start_urls = ["http://money.163.com/stock/"]
     
     custom_settings={'ITEM_PIPELINES':
